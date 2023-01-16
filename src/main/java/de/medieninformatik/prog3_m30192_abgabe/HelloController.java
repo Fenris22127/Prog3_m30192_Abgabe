62ffff1c5598a2003197d4f95aae13924c0ffd7c
Elisa <elisa-johanna.woelk@outlook.de>
2023-01-16 23:55:39 +0100

    Initial commit

---
+++ src/main/java/de/medieninformatik/prog3_m30192_abgabe/HelloController.java
@@ -0,0 +1,14 @@
+package de.medieninformatik.prog3_m30192_abgabe;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module de.medieninformatik.prog3_m30192_abgabe {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens de.medieninformatik.prog3_m30192_abgabe to javafx.fxml;
+    exports de.medieninformatik.prog3_m30192_abgabe;
+}
